a9c15e2f58e4554db8c2a48fef8bca09e60a5b5b
jvpts11 <jvptsyt11@gmail.com>
2023-01-18 13:29:55 +0000

    you can now insert books
    do whatever you want with them

--- Ipca Smart Library Interface/WindowsFormsApp1/Book_Query.cs
+++ Ipca Smart Library Interface/WindowsFormsApp1/Book_Query.cs
@@ -27,6 +27,18 @@ namespace WindowsFormsApp1
         {
             SqlConnection con = new SqlConnection(@"Data Source=JOTAPC\AULAS;Initial Catalog=IPCA_SMART_LIBRARY;User ID=sa;Password=123456");
             con.Open();
+
+            SqlCommand command = new SqlCommand("insert into dbo.Book values(@BKId,@BKName,@TypeId,@BORId,null,@EDTId,null,null)", con);
+            command.Parameters.Add("@BKId", int.Parse(book_id_input.Text));
+            command.Parameters.Add("@BKName", book_name_input.Text);
+            command.Parameters.Add("@TypeId", int.Parse(book_type_id_input.Text));
+            command.Parameters.Add("@BORId", int.Parse(books_buy_order_id_input.Text));
+            command.Parameters.Add("@EDTId", int.Parse(editor_id_input.Text));
+            command.ExecuteNonQuery();
+            
+            con.Close();
+
+            MessageBox.Show("Success!");
         }
 
         private void update_button_Click(object sender, EventArgs e)
